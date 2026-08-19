43a0b570ce37de8e1c1e8469a95fa22186ffdacc
iZafarAhmed <126581461+iZafarAhmed@users.noreply.github.com>
2026-08-19 22:41:16 +0530

    Update VideoAdapter.kt

--- app/src/main/java/com/example/youtuberssreader/VideoAdapter.kt
+++ app/src/main/java/com/example/youtuberssreader/VideoAdapter.kt
@@ -1,5 +1,7 @@
 package com.example.youtuberssreader
 
+import com.example.youtuberssreader.R  // <--- ADD THIS LINE
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
