e9de4801c2374d5a5765018cd1d2e1d009ecc737
iZafarAhmed <126581461+iZafarAhmed@users.noreply.github.com>
2026-08-19 20:27:32 +0530

    Add Video data class for YouTube RSS reader

---
+++ src/main/java/com/example/youtuberssreader/Video.kt
@@ -0,0 +1,10 @@
+package com.example.youtuberssreader
+
+data class Video(
+    var id: String = "",
+    var title: String = "",
+    var thumbnail: String = "",
+    var published: String = "",
+    var views: String = "0",
+    var channelTitle: String = ""
+)
